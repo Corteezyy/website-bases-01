d02e0f1d75343d2cc47eedb849dbf48a8ba05ca7
John <naranjo699@yahoo.com>
2021-02-08 22:00:08 -0800

    My first commit!

---
+++ index.js
@@ -0,0 +1,10 @@
+// This is your first javascript file. Javascript will help us make our website interactive.
+
+// This is a console log message. This will print out in the console.
+console.log('This is a logged message!');
+
+// Get element by ID
+document.getElementById("app");
+
+// Get element by Class
+document.getElementsByClassName('foo');
